c646ba5d0914c35013bd8ea9fccae69d5656c1fb
Ashish Singh Rawat <ashish9342@gmail.com>
2018-09-28 00:33:27 +0530

    book reduces added

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,15 @@
+export default function() {
+    return [{
+            title: 'Javascript: the Good parts'
+        },
+        {
+            title: 'The Dark  Night'
+        },
+        {
+            title: 'Eloquent JS'
+        },
+        {
+            title: 'You Dont know js' 
+        }
+    ]
+}
