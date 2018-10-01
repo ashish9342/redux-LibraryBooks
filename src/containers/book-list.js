5cc7b44a0264d2a3aae1ecd25f85d6a900db4df9
Ashish Singh Rawat <ashish9342@gmail.com>
2018-10-01 21:01:45 +0530

    book-deatil container added

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from  '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class Booklist extends Component{
 
     renderList(){
         return this.props.books.map((book)=>{
             return (
-                <li key= {book.title} className="list-group-item">{book.title}</li>
+                <li 
+                key= {book.title} 
+                onClick={()=> this.props.selectBook(book)}
+                className="list-group-item">{book.title}</li>
             )
         });
     }
@@ -30,4 +35,20 @@ function mapStateToProps(state){
 
 }
 
-export default connect(mapStateToProps)(Booklist);
+// mapDispatchToProps
+// Anything returned from this function will end as props
+// on th BookList container
+function mapDispatchToProps(dispatch){
+
+
+    // bindActionCreators() 
+    // whenever selectbook is callled , the result should be passed
+    // to all of our reducers
+    return bindActionCreators({selectBook:selectBook}, dispatch)
+
+}
+
+// Promote boolist from a component to a container
+// it needs to know about thisnew dispatch method, select book and
+// make it available as props
+export default connect(mapStateToProps, mapDispatchToProps)(Booklist);
